136476e921be3f49719d50f243ac18baa6b7bed0
prod-i <prodi.ofic@gmail.com>
2021-06-08 13:03:37 +0300

    hover views series

--- src/common/SerialsPage/Components/PlayerModal.jsx
+++ src/common/SerialsPage/Components/PlayerModal.jsx
@@ -7,12 +7,16 @@ const PlayerModal = (props) => {
     const item = props.item;
     return (
             <div className="player_modal" key={props.key}>
-                <div className="player_modal_title tW fCG"><span className="tR">{props.title} - </span> {item.title}</div>
+                <div className="player_modal_title tW fCG"><span className="tRP">{props.title} - </span> {item.title}</div>
                 <div className="player_modal_videoplayer bD">
                     <VideoPlayer />
                 </div>
                 <div className="player_modal_options tR">
-                <div className="player_modal_space"></div><EyeOutlined />
+                <div className="player_modal_space"></div>
+                <div className="player_modal-views">
+                    <EyeOutlined />
+                    <div className="player_modal-views-hover fCG tA">10002 views</div>
+                </div>
                 </div>
             </div>
     )
