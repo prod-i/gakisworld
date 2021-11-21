8f89ba424f3c725cf3ddff073f4ea6879ed1cd52
prod-i <prodi.ofic@gmail.com>
2021-11-21 16:09:57 +0300

    delete setTimeout for Catalog

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,37 +1,31 @@
-import React        from 'react';
-import Novelty      from './Novelty/Novelty';
-import Search       from './Search/Search';
-import CatalogList  from './CatalogList/CatalogList';
-import Footer       from '../../common/Footer/Footer';
+import React from 'react';
+import Novelty from './Novelty/Novelty';
+import Search from './Search/Search';
+import CatalogList from './CatalogList/CatalogList';
+import Footer from '../../common/Footer/Footer';
 import '../../style/catalog/catalog.css'
 import ScrollTop from '../../common/ScrollTop/ScrollTop';
-import Preloader from '../../common/Preloader/Preloader';
+// import Preloader from '../../common/Preloader/Preloader';
 
 
-const Catalog = (props)=> {
-        const [check, setCheck] = React.useState(false);
-        setTimeout(()=>setCheck(true), 500)
-        if(check){
-            return(
-            
-                <div className={props.catalog.catalogVis ? 'catalog' : 'none'}>
-    
-                    <Novelty novelty={props.catalog.novelty} catalog={props.catalog} width={props.width}/>
-    
-                    <Search search={props.search} ChangeSearchValue={props.ChangeSearchValue} Changefiltre={props.Changefiltre}/>
-    
-                    <CatalogList catalog={props.catalog} searchInput={props.search.searchInput} width={props.width}/>
-    
-                    <Footer />
-    
-                    <ScrollTop scrollTo={970}/>
-                    
-                    
-                </div>
-            );
-        } else {
-            return <Preloader check/>
-        }
+const Catalog = (props) => {
+    return (
+
+        <div className={props.catalog.catalogVis ? 'catalog' : 'none'}>
+
+            <Novelty novelty={props.catalog.novelty} catalog={props.catalog} width={props.width} />
+
+            <Search search={props.search} ChangeSearchValue={props.ChangeSearchValue} Changefiltre={props.Changefiltre} />
+
+            <CatalogList catalog={props.catalog} searchInput={props.search.searchInput} width={props.width} />
+
+            <Footer />
+
+            <ScrollTop scrollTo={970} />
+
+
+        </div>
+    );
 };
 
 export default Catalog;
